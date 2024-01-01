388da68e1abb1665da774e2132b786dc8c891089
Richard Carter <goobysc@gmail.com>
2024-01-01 17:08:04 -0500

    change allow addresses

--- software/fah.nix
+++ software/fah.nix
@@ -5,6 +5,9 @@
         enable = true;
         user = "rcarte4";
         team = 60194;
+        extraArgs = [
+            "--allow='10.0.0.1-10.0.3.254'"
+        ]
     };
 
     networking.firewall.allowedTCPPorts = [ 7396 ];
