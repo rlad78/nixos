3fe83ba498badb8c0be9f31622b0317f8d27a091
Richard Carter <goobysc@gmail.com>
2024-01-01 17:22:52 -0500

    banging head against fah

--- software/fah.nix
+++ software/fah.nix
@@ -6,7 +6,8 @@
         user = "rcarte4";
         team = 60194;
         extraArgs = [
-            "--web-allow=10.0.0.1/22"
+            "--allow=10.0.0.1-10.0.3.254"
+            "--web-allow=10.0.0.1-10.0.3.254"
         ];
     };
 
