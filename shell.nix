8fe60eb7b130b629fc72ce6160b1246c6e25cf37
Richard Carter <goobysc@gmail.com>
2024-01-01 20:17:17 -0500

    add nxupd alias

--- shell.nix
+++ shell.nix
@@ -39,6 +39,7 @@
         ls = "lsd";
         ll = "lsd -l";
         la = "lsd -la";
+        nxupd = "cd /users/richard/nixos/ && gh repo sync";
         nxr = "nixos-rebuild switch --flake .#default";
         nxb = "nixos-rebuild boot --flake .#default";
         nxt = "nixos-rebuild test --flake .#default";
