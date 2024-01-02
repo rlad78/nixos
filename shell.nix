ff195b9f84b0b038b2d85d1a6d08a43cb393210a
Richard Carter <goobysc@gmail.com>
2024-01-01 20:34:11 -0500

    add pubkey

--- shell.nix
+++ shell.nix
@@ -27,6 +27,9 @@
             xonsh
         ];
         hashedPassword = "$6$SkIi1e6zfsLHIUvR$Xg3ZYvL5EsEh/jzcvHX2s6O0a5Z7RmyWRyeLGMMsh6XJnCcTZmrM4EC4N0n08WlIiJP2radM56K6UpLXvb122/";
+        openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc2jzGXOAiQxBeec8qe8cqemg5O1/uCC/OWEDFJFvax richard@silverblue-go"
+        ];
     };
 
     users.users.richard.shell = pkgs.zsh;
