a52df115312c28f00934d78b05f24b7dd3256013
Richard Carter <goobysc@gmail.com>
2024-01-01 20:30:22 -0500

    multiple alias for gh repo sync

--- shell.nix
+++ shell.nix
@@ -34,12 +34,16 @@
 
 
     #shell settings
+    let
+        update-cmd = "cd /home/richard/nixos/ && gh repo sync";
+    in
     environment.shellAliases = {
         sudo = "sudo ";
         ls = "lsd";
         ll = "lsd -l";
         la = "lsd -la";
-        nxupd = "cd /users/richard/nixos/ && gh repo sync";
+        nxupd = update-cmd;
+        nxupg = update-cmd;
         nxs = "nixos-rebuild switch --flake .#default";
         nxb = "nixos-rebuild boot --flake .#default";
         nxt = "nixos-rebuild test --flake .#default";
