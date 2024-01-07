0712213fe88eacef1e71d3ee1ed588b9f27e2715
Richard Carter <goobysc@gmail.com>
2024-01-07 15:35:33 -0500

    add rpc whitelist

--- software/torrent.nix
+++ software/torrent.nix
@@ -15,8 +15,12 @@
         home = "/storage/torrents";
         downloadDirPermissions = "775";
 
+        # these can be any of the Transmission settings.json entries
+        # https://github.com/transmission/transmission/blob/main/docs/Editing-Configuration-Files.md
         settings = {
+            rpc-enabled = true;
             rpc-bind-address = "0.0.0.0";
+            rpc-whitelist = "10.0.0.*,10.0.1.*,10.0.2.111,10.0.3.*";
         };
         
         openRPCPort = true;
