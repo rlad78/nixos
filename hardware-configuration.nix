e11954e6918136639e4687bf89a2f30eafd32a7d
Richard Carter <goobysc@gmail.com>
2024-01-07 14:51:18 -0500

    mount 1TB drive to /storage

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -23,6 +23,16 @@
       fsType = "vfat";
     };
 
+  # STORAGE DISKS
+  users.groups.storage.members = [ "richard" "syncthing" ];
+  
+  ## 1TB spinning rust
+  systemd.tmpfiles.rules = [ "d /storage 0775 root storage" ];
+  fileSystems."/storage" =
+    { device = "/dev/disk/by-uuid/0082b0dd-ef0c-4928-84d6-f9206600aa4b";
+      fsType = "ext4";
+    };
+
   swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
