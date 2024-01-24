9f96cbe531c74c8c05cdb01d599dca1e2af31c69
Richard Carter <goobysc@gmail.com>
2024-01-24 09:18:17 -0500

    add non-tail ips to host file

--- tailscale.nix
+++ tailscale.nix
@@ -3,6 +3,8 @@
 let
   has-tail-ip = with lib.attrsets; filterAttrs (n: v: hasAttrByPath ["tail-ip"] v);
   tail-hosts = with lib.attrsets; concatMapAttrs (n: v: { ${v.tail-ip} = lib.lists.singleton n; } );
+  has-local-only-ip = with lib.attrsets; filterAttrs (n: v: (hasAttr "local-ip" v) && !(hasAttr "tail-ip" v));
+  local-hosts = with lib.attrsets; concatMapAttrs (n: v: { ${v.local-ip} = lib.lists.singleton n; } );
 in
 {
   services.tailscale.enable = true;
@@ -13,5 +15,9 @@ in
   # networking.firewall.allowedTCPPorts = [ ... ];
   networking.firewall.allowedUDPPorts = [ 41641 ];
 
-  networking.hosts = tail-hosts (has-tail-ip (removeAttrs me.hosts [ uncommon.host ]));
+  networking.hosts = (
+    tail-hosts (has-tail-ip (removeAttrs me.hosts [ uncommon.host ])) 
+    # also include non-tailscale hosts
+    // (local-hosts (has-local-only-ip (removeAttrs me.hosts [ uncommon.host ])))
+  );
 }
