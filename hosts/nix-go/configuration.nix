196425c224c4146b2d2845c42864dcaf277882c1
Richard Carter <goobysc@gmail.com>
2024-01-23 19:41:40 -0500

    add powerprofilectl

--- hosts/nix-go/configuration.nix
+++ hosts/nix-go/configuration.nix
@@ -14,6 +14,9 @@
       ./apps.nix
     ];
 
+  # enable powerprofilesctl
+  services.power-profiles-daemon.enable = true;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
