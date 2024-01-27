67198f2ce5a1ce68ae837d33aedd8c7450937d9e
Richard Carter <goobysc@gmail.com>
2024-01-27 10:58:13 -0500

    forgot to add my full name back

--- richard.nix
+++ richard.nix
@@ -9,6 +9,7 @@
   users.users.richard = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    description = "Richard Lee Carter Jr";
     hashedPassword = "$6$SkIi1e6zfsLHIUvR$Xg3ZYvL5EsEh/jzcvHX2s6O0a5Z7RmyWRyeLGMMsh6XJnCcTZmrM4EC4N0n08WlIiJP2radM56K6UpLXvb122/";
     openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc2jzGXOAiQxBeec8qe8cqemg5O1/uCC/OWEDFJFvax richard@silverblue-go"
