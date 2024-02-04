cc735c9da71c46ac660b6672e910532a76c763f9
Richard Carter <goobysc@gmail.com>
2024-02-04 17:09:21 -0500

    static container definition

--- snootflix/indexers.nix
+++ snootflix/indexers.nix
@@ -61,8 +61,50 @@ in
       (snootflix.mkConfDir "sonarr_tv")
     ];
     
-    containers.sonarr_anime = mkSonarrContainer "sonarr_anime" "69" 8981;
-    containers.sonarr_tv = mkSonarrContainer "sonarr_tv" "79" 8982;
+    # containers.sonarr_anime = mkSonarrContainer "sonarr_anime" "69" 8981;
+    # containers.sonarr_tv = mkSonarrContainer "sonarr_tv" "79" 8982;
+
+    containers.sonarr_anime = {
+      privateNetwork = true;
+      hostAddress = "192.168.69.2";
+      localAddress = "192.168.69.10";
+      forwardPorts = [{
+        containerPort = 8989;
+        hostPort = 8981;
+        protocol = "tcp";
+      }];
+
+      autoStart = true;
+      ephemeral = true;
+      bindMounts = {
+        "/host/config" = {
+          hostPath = snootflix.mkConfPath [ "sonarr_anime" ];
+          isReadOnly = false;
+        };
+        "/host/snootflix" = {
+          hostPath = snootflix.dirs.main;
+          isReadOnly = false;
+        };
+      };
+      
+      config = { config, pkgs, ... }: {
+        users.groups."snootflix" = { gid=6969; };
+        systemd.tmpfiles.rules = [
+          "d /host/config 0770 sonarr snootflix"
+          "d /host/snootflix 0770 sonarr snootflix"
+        ];
+
+        services.sonarr = {
+          enable = true;
+          openFirewall = true;
+          group = "snootflix";
+          dataDir = "/host/config";
+        };
+        
+        system.stateVersion = "23.11";
+        networking.useHostResolvConf = lib.mkForce false;
+      };
+    }
 
     networking = {
       nat = {
