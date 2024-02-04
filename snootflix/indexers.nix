2b804879f5e28eb0ae3246528d70d64a532b5941
Richard Carter <goobysc@gmail.com>
2024-02-04 17:01:44 -0500

    supress snootflix group

--- snootflix/indexers.nix
+++ snootflix/indexers.nix
@@ -24,7 +24,7 @@ let
     # };
     
     config = { config, pkgs, ... }: {
-      users.groups."${snootflix.group}" = { gid=6969; };
+      # users.groups."${snootflix.group}" = { gid=6969; };
       # systemd.tmpfiles.rules = [
       #   "d /host/config 0770 sonarr ${snootflix.group}"
       #   "d /host/snootflix 0770 sonarr ${snootflix.group}"
