9bbe66bbf531a4d6e25473c3ca4ea91e43512457
Richard Carter <goobysc@gmail.com>
2024-02-06 19:47:30 -0500

    try packaged retroarch

--- apps/games.nix
+++ apps/games.nix
@@ -1,13 +1,15 @@
 { configs, pkgs, ... }:
 {
   users.users.richard.packages = with pkgs; [
+    retroarch-with-cores
+    retroarch-assets-unstable
     space-cadet-pinball
     typespeed
   ];
 
-  services.flatpak.packages = [
-    "org.libretro.RetroArch"
-  ];
+  # services.flatpak.packages = [
+    # "org.libretro.RetroArch"
+  # ];
 
   programs.gamemode.enable = true;
 }
