a042cb3e0a6ea491fc5da09b11fec06b03a75249
Richard Carter <goobysc@gmail.com>
2024-02-10 20:45:48 -0500

    swap back retroarch for flatpak version

--- apps/games.nix
+++ apps/games.nix
@@ -1,15 +1,13 @@
 { configs, pkgs, ... }:
 {
   users.users.richard.packages = with pkgs; [
-    retroarchFull
-    retroarch-assets
     space-cadet-pinball
     typespeed
   ];
 
-  # services.flatpak.packages = [
-    # "org.libretro.RetroArch"
-  # ];
+  services.flatpak.packages = [
+    "org.libretro.RetroArch"
+  ];
 
   programs.gamemode.enable = true;
 }
