21ea52d29fdfa1abefc914b3d72acd65a8dd9d29
Richard Carter <goobysc@gmail.com>
2024-02-19 18:14:40 -0500

    add bubblemail and systemd user service

--- apps/utils.nix
+++ apps/utils.nix
@@ -2,10 +2,24 @@
 {
   users.users.richard.packages = with pkgs; [
     bitwarden
+    bubblemail
+    gnomeExtensions.bubblemail
   ];
 
   services.flatpak.packages = [
     "com.github.tchx84.Flatseal"
     "com.cassidyjames.butler"
   ];
+
+  systemd.user.services.bubblemail = {
+    enable = true;
+    description = "Start bubblemaild service";
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      StandardOutput = "journal";
+      ExecStart = "${pkgs.bubblemail}/bin/bubblemaild";
+    };
+    wantedBy = [ "default.target" ];
+  };
 }
