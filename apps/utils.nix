36b357b6ac68f05d70be620bd685b9ca533e605d
Richard Carter <goobysc@gmail.com>
2024-02-19 18:14:26 -0500

    add bitwarden

--- apps/utils.nix
+++ apps/utils.nix
@@ -1,6 +1,8 @@
 { configs, pkgs, ... }:
 {
-  users.users.richard.packages = with pkgs; [];
+  users.users.richard.packages = with pkgs; [
+    bitwarden
+  ];
 
   services.flatpak.packages = [
     "com.github.tchx84.Flatseal"
