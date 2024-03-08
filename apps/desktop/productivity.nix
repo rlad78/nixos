0365541ac3675938b370ab6416faed41e4524dfd
Richard Carter <goobysc@gmail.com>
2024-03-08 16:09:20 -0500

    add libreoffice

--- apps/desktop/productivity.nix
+++ apps/desktop/productivity.nix
@@ -1,5 +1,9 @@
 { configs, pkgs, ... }:
 {
+  users.users.richard.packages = with pkgs; [
+    libreoffice-fresh
+  ];
+
   services.flatpak.enable = true;
   services.flatpak.update.onActivation = true;
 
