68e4710f6d9dfdaa8744ee6866f73bdfa5df2f2f
Richard Carter <goobysc@gmail.com>
2024-03-13 20:50:08 -0400

    re-enable service

--- services/ai.nix
+++ services/ai.nix
@@ -1,9 +1,9 @@
 { config, pkgs, nixified-ai, ... }:
 {
-  # imports = [ nixified-ai.nixosModules.invokeai-nvidia ];
+  imports = [ nixified-ai.nixosModules.invokeai-nvidia ];
 
   nix.settings.trusted-substituters = ["https://ai.cachix.org"];
   nix.settings.trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
 
-  # services.invokeai.enable = true;
+  services.invokeai.enable = true;
 }
