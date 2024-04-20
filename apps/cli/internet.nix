fa6572ecfa7a097c5e0a333151d133395a89808d
Richard Carter <goobysc@gmail.com>
2024-04-20 18:04:24 -0400

    add termshark

--- apps/cli/internet.nix
+++ apps/cli/internet.nix
@@ -4,5 +4,15 @@
     gh
     lazygit
     aria2
+    termshark
   ];
+
+  users.users.richard.extraGroups = [ "wireshark" ];
+
+  security.wrappers.termshark = {
+    owner = "root";
+    group = "root";
+    source = "${pkgs.termshark}/bin/termshark";
+    capabilities = "cap_net_raw,cap_net_admin+eip";
+  };
 }
