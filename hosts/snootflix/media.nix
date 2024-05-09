b878c1f3b555c57f91d4707eebe2f42a040ef026
Richard Carter <goobysc@gmail.com>
2024-05-09 08:23:06 -0400

    add buildarr python script

--- hosts/snootflix/media.nix
+++ hosts/snootflix/media.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, hosts, ... }:
+{ config, pkgs, lib, hosts, mkPoetryScriptsPackage, ... }:
 let
   mnt-prefix = "mediadisk";
   mergerfs-dir = "/snoot";
@@ -7,7 +7,7 @@ let
     "3b5ccd1e-00ea-4dac-b27f-1c7822c737c4"
     "2f2168da-b5d0-4b06-bbb4-e70b042a412f"
     "feea2c5b-65c4-4368-8960-ca84fcaf1d42"
-    "bd370bec-2c57-4c20-ac85-98a40421a3a0"
+    "ba7fc5c0-df48-4a5a-b20a-7e0b653b6060"
   ] (x: "/dev/disk/by-uuid/${x}");
 
   # -----
@@ -24,6 +24,134 @@ let
       )
     )
   );
+
+  # buildarr-src = pkgs.fetchFromGitHub {
+    # owner = "buildarr";
+    # repo = "buildarr";
+    # rev = "093bd02e8269e05b8c5c56cffa756a9b549ebccc";
+    # hash = "sha256-it5zrpsf6ybBznXgs8I9El1tArV9m5jKkQ7R0yBiDFc=";
+  # };
+#
+  # buildarr = mkPoetryScriptsPackage {
+    # projectDir = buildarr-src;
+    # python = pkgs.python311;
+  # };
+
+  validators-pypi = {
+    buildPythonPackage,
+    fetchPypi,
+    setuptools,
+  }: ( buildPythonPackage rec {
+    pname = "validators";
+    version = "0.22.0";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-d7Jomxcu7rYA2WBauGGUZBZwzbc7YK/VdxQqk5eHM3A=";
+    };
+    doCheck = false;
+    pyproject = true;
+    build-system = [ setuptools ];
+  });
+
+  validators = pkgs.python311Packages.callPackage validators-pypi {};
+
+  click-params-pypi = {
+    buildPythonPackage,
+    fetchPypi,
+    poetry-core,
+    click,
+    # validators,
+    deprecated
+  }: ( buildPythonPackage rec {
+    pname = "click_params";
+    version = "0.5.0";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-X+l7lFl4GjtDuE/k7ABlGT4bDVz23HeJf+IMMfR41/8=";
+    };
+    doCheck = false;
+    pyproject = true;
+    build-system = [ poetry-core ];
+    propagatedBuildInputs = [
+      click
+      validators
+      deprecated
+    ];
+  });
+
+  click-params = pkgs.python311Packages.callPackage click-params-pypi {};
+
+  # pydantic-pypi = {
+    # buildPythonPackage,
+    # fetchPypi,
+    # setuptools,
+    # devtools,
+    # email-validator,
+    # typing-extensions,
+    # python-dotenv
+  # }: ( buildPythonPackage rec {
+    # pname = "pydantic";
+    # version = "1.10.10";
+    # src = fetchPypi {
+      # inherit pname version;
+      # hash = "sha256-O41b2XiG+etZJgWUIHyfV9zhSm+GnGzuqQGIcV0pkho=";
+    # };
+    # doCheck = false;
+    # pyproject = false;
+    # build-system = [ setuptools ];
+    # propagatedBuildInputs = [
+      # devtools
+      # email-validator
+      # typing-extensions
+      # python-dotenv
+    # ];
+  # });
+#
+  # pydantic = pkgs.python311Packages.callPackage pydantic-pypi {};
+
+  buildarr-pypi = { 
+    buildPythonApplication, 
+    fetchPypi, 
+    setuptools, 
+    setuptools-scm, 
+    poetry-core,
+    aenum,
+    click,
+    # click-params,
+    importlib-metadata,
+    pydantic_1,
+    pyyaml,
+    requests,
+    schedule,
+    stevedore,
+    watchdog,
+  }: ( buildPythonApplication rec {
+    pname = "buildarr";
+    version = "0.7.1";
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-FmYFChehwNYK7D+wBujuEBl02SxdKD7UBC7jGReeqnM==";
+    };
+    doCheck = false;
+    pyproject = true;
+    build-system = [
+      setuptools
+      setuptools-scm
+      poetry-core
+    ];
+    propagatedBuildInputs = [
+      aenum
+      click
+      click-params
+      importlib-metadata
+      pydantic_1
+      pyyaml
+      requests
+      schedule
+      stevedore
+      watchdog
+    ];
+  });
 in
 {
   nixarr = {
@@ -68,6 +196,9 @@ in
     mergerfs
     mergerfs-tools
     recyclarr
+    # (python311.withPackages (python-pkgs: [
+      (python311Packages.callPackage buildarr-pypi {})
+    # ]))
   ];
 
   fileSystems = with lib; attrsets.mergeAttrsList (
