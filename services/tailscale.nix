7fa476d8865f36cd5544408195c676ee7b158ff8
Richard Carter <goobysc@gmail.com>
2024-05-23 19:54:14 -0400

    force tailscale to always be from unstable

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,5 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 {
-  services.tailscale.enable = true;
+  imports = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/programs/nh.nix"
+  ];
+
+  services.tailscale = {
+    enable = true;
+    package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.tailscale;
+  };
+
   networking.firewall.allowedUDPPorts = [ 41641 ];
 }
