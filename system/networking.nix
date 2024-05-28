181d49458f93f4dd2292dd5ec1d5426b7998b75c
Richard Carter <goobysc@gmail.com>
2024-05-28 13:51:51 -0400

    assign default interfaces for container net binding

--- system/networking.nix
+++ system/networking.nix
@@ -1,9 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, hosts, ... }:
 {
   # fix for nm-wait-online (https://github.com/NixOS/nixpkgs/issues/180175)
   systemd.services.NetworkManager-wait-online = {
     serviceConfig = {
       ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q" ];
     };
+  };
+
+  networking.nat = {
+    enable = true;
+    internalInterfaces = ["ve-+"];
+    externalInterface = hosts."${config.networking.hostName}".default-net-dev;
+    # Lazy IPv6 connectivity for the container
+    enableIPv6 = true;
   };
 }
