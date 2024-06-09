069e32d19d357eeb7c6564619e3d6bcf13a71ae9
Richard Carter <goobysc@gmail.com>
2024-06-09 14:23:37 -0400

    use standard tailscale config

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,13 +1,7 @@
 { config, pkgs, inputs, ... }:
 {
-  imports = [
-    "${inputs.nixpkgs-unstable}/nixos/modules/programs/nh.nix"
-  ];
-
   services.tailscale = {
     enable = true;
-    package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.tailscale;
+    openFirewall = true;
   };
-
-  networking.firewall.allowedUDPPorts = [ 41641 ];
 }
