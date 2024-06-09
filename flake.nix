663608c03fae7c63a7270f03e8cf155deeed7908
Richard Carter <goobysc@gmail.com>
2024-06-09 13:49:55 -0400

    update nixpkgs to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.1.0";
