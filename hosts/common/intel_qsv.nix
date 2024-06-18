093acb88e6f55b2efdc13713250e110a803fb9b1
Richard Carter <goobysc@gmail.com>
2024-06-18 16:02:16 -0400

    more fixes for qsv

--- hosts/common/intel_qsv.nix
+++ hosts/common/intel_qsv.nix
@@ -4,11 +4,24 @@
     "i915.enable_guc=2"
   ];
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
       intel-compute-runtime
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
     ];
   };
+
+  environment.systemPackages = with pkgs; [
+    intel-gpu-tools
+  ];
+
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 }
