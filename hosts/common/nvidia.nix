29d8feeb5147d56cba0c43517ed1d1721f07c69c
Richard Carter <goobysc@gmail.com>
2024-06-24 10:35:20 -0400

    fix tabbing on nvidia.nix

--- hosts/common/nvidia.nix
+++ hosts/common/nvidia.nix
@@ -1,28 +1,28 @@
 { config, pkgs, lib, ... }:
 
 {
-    environment.systemPackages = with pkgs; [
-        cudatoolkit
-    ];
+  environment.systemPackages = with pkgs; [
+    cudatoolkit
+  ];
 
-    services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-    hardware.opengl = {
-        enable = true;
-        driSupport = true;
-        driSupport32Bit = true;
-    };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 
-    hardware.nvidia = {
-        # Modesetting is required.
-        modesetting.enable = true;
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
 
-        # independent third-party "nouveau" open source driver).
-        open = false;
+    # independent third-party "nouveau" open source driver).
+    open = false;
 
-        # nvidia-persistenced a update for NVIDIA GPU headless mode 
-        # i.e. It ensures all GPUs stay awake even during headless mode
-        nvidiaPersistenced = true;
-    };
+    # nvidia-persistenced a update for NVIDIA GPU headless mode 
+    # i.e. It ensures all GPUs stay awake even during headless mode
+    nvidiaPersistenced = true;
+  };
 }
