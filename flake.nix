9613381be7d2d35c486bdd6f9a40ed201a0ca393
Richard Carter <goobysc@gmail.com>
2024-11-02 13:42:39 -0400

    new flake format

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.1.0";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
-    pomatez-flake.url = "github:rlad78/pomatez-flake";
+    # pomatez-flake.url = "github:rlad78/pomatez-flake";
 
     nixarr = {
       url = "github:rlad78/nixarr/prod";
@@ -15,11 +15,41 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, nix-flatpak, nix-vscode-extensions, nixarr, pomatez-flake, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, nix-flatpak, nix-vscode-extensions, nixarr, ... }@inputs:
   let
     secrets = builtins.fromJSON (builtins.readFile "${self}/secrets/secrets.json");
     hosts = builtins.fromJSON (builtins.readFile "${self}/secrets/hosts.json");
     builders = builtins.fromJSON (builtins.readFile "${self}/system/builders.json");
+
+    host-conf-options = {
+      nix-go = {
+        pkg-base = nixpkgs-unstable;
+        special-inherits = {
+          inherit nix-flatpak;
+          inherit nix-vscode-extensions;
+        };
+        module-paths = [
+          ./hosts/nix-go
+          nixos-hardware.nixosModules.microsoft-surface-pro-intel
+          nix-flatpak.nixosModules.nix-flatpak
+        ];
+      };
+
+      nixarf = {
+        pkg-base = nixpkgs;
+        special-inherits = {};
+        module-paths = [ ./hosts/nixarf ];
+      };
+
+      snootflix = {
+        pkg-base = nixpkgs-unstable;
+        special-inherits = {};
+        module-paths = [
+          ./hosts/snootflix
+          nixarr.nixosModules.default
+        ];
+      };
+    };
 
     standardArgs = nixpkgs-input: {
       inherit inputs;
@@ -28,61 +58,29 @@
       inherit builders;
       util = import ./util.nix nixpkgs-input.lib;
     };
+
+    pkgsBuild = base: import base {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+
+    systemMake = host: host.pkg-base.lib.nixosSystem {
+      specialArgs = rec {
+        pkgs = pkgsBuild host.pkg-base;
+        pkgs-unstable = 
+          if host.pkg-base == nixpkgs-unstable
+          then pkgs
+          else pkgsBuild nixpkgs-unstable;
+        } // (host.special-inherits) // (standardArgs host.pkg-base);
+
+      modules = host.module-paths;
+    };
   in
   {
     nixosConfigurations = {
-      
-      nixarf = nixpkgs.lib.nixosSystem {  
-        specialArgs = rec {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          pkgs-unstable = import nixpkgs-unstable {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-        } // (standardArgs nixpkgs);
-        
-        modules = [ 
-          ./hosts/nixarf
-        ];
-      };
-
-      "nix-go" = nixpkgs.lib.nixosSystem {
-        specialArgs = rec {
-          pkgs = import nixpkgs {
-	          system = "x86_64-linux";
-	          config.allowUnfree = true;
-	        };
-          inherit nix-flatpak;
-          inherit nix-vscode-extensions;
-          pkgs-unstable = import nixpkgs-unstable {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-	      } // (standardArgs nixpkgs);
-
-    	  modules = [
-	        ./hosts/nix-go
-          nixos-hardware.nixosModules.microsoft-surface-pro-intel
-          nix-flatpak.nixosModules.nix-flatpak
-	      ];
-      };
-
-      snootflix = nixpkgs-unstable.lib.nixosSystem {
-        specialArgs = rec {
-          pkgs = import nixpkgs-unstable {
-            system = "x86_64-linux";
-	          config.allowUnfree = true;
-          };
-        } // (standardArgs nixpkgs-unstable);
-
-        modules = [
-          ./hosts/snootflix
-          nixarr.nixosModules.default
-        ];
-      };
+      nixarf = systemMake host-conf-options.nixarf;
+      nix-go = systemMake host-conf-options.nix-go;
+      snootflix = systemMake host-conf-options.snootflix;
     };
   };
 }
