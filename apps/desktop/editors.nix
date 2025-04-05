559459ed4c227bee3c279f3c69fb662a466c8aee
Richard Carter <goobysc@gmail.com>
2025-04-05 11:02:24 -0400

    temp remove vscode

--- apps/desktop/editors.nix
+++ apps/desktop/editors.nix
@@ -3,25 +3,25 @@ let
   extensions = nix-vscode-extensions.extensions.${pkgs.system};
 in
 {
-  users.users.richard.packages = with pkgs; [
-    (vscode-with-extensions.override {
-      vscodeExtensions = with extensions.vscode-marketplace; [
-        ms-python.python
-        aaron-bond.better-comments
-        codezombiech.gitignore
-        mathematic.vscode-pdf
-        mechatroner.rainbow-csv
-        ms-python.black-formatter
-        ms-python.isort
-        ms-python.vscode-pylance
-        njpwerner.autodocstring
-        piotrpalarz.vscode-gitignore-generator
-        tamasfe.even-better-toml
-        teabyii.ayu
-        jnoortheen.nix-ide
-      ];
-    })
-  ];
+  # users.users.richard.packages = with pkgs; [
+    # (vscode-with-extensions.override {
+      # vscodeExtensions = with extensions.vscode-marketplace; [
+        # ms-python.python
+        # aaron-bond.better-comments
+        # codezombiech.gitignore
+        # mathematic.vscode-pdf
+        # mechatroner.rainbow-csv
+        # ms-python.black-formatter
+        # ms-python.isort
+        # ms-python.vscode-pylance
+        # njpwerner.autodocstring
+        # piotrpalarz.vscode-gitignore-generator
+        # tamasfe.even-better-toml
+        # teabyii.ayu
+        # jnoortheen.nix-ide
+      # ];
+    # })
+  # ];
 
   environment.systemPackages = with pkgs; [
     poetry
