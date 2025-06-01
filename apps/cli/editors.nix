c9ac8a5ec0c809275a2aafc6c3144178339f3e4d
Richard Carter <goobysc@gmail.com>
2025-06-01 12:45:28 -0400

    transparency and colorscheme option

--- apps/cli/editors.nix
+++ apps/cli/editors.nix
@@ -1,64 +1,102 @@
-{ configs, pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
+let
+  cfg = config.arf.nixvim;
+  plug-on = { enable = true; autoLoad = true; };
+in
 {
-  programs.nixvim = {
-    enable = true;
-    defaultEditor = true;
-    vimAlias = true;
-    extraConfigVim = ''
-      set tabstop=2
-      set shiftwidth=2
-      set expandtab
-      set smartindent
-      set rnu
-    '';
+  options.arf.nixvim = with lib; {
+    colorscheme = mkOption {
+      type = types.enum [
+        "ayu"
+        "bamboo"
+        "base16"
+        "catppuccin"
+        "cyberdream"
+        "dracula"
+        "dracula-nvim"
+        "everforest"
+        "github-theme"
+        "gruvbox"
+        "kanagawa"
+        "kanagawa-paper"
+        "melange"
+        "modus"
+        "monokai-pro"
+        "nightfox"
+        "nord"
+        "one"
+        "onedark"
+        "oxocarbon"
+        "palette"
+        "poimandres"
+        "rose-pine"
+        "tokyonight"
+        "vscode"
+      ];
+      default = "cyberdream";
+    };
+  };
 
-    colorschemes.catppuccin.enable = true;
+  config = {
+    programs.nixvim = {
+      enable = true;
+      defaultEditor = true;
+      vimAlias = true;
+      extraConfigVim = ''
+        set tabstop=2
+        set shiftwidth=2
+        set expandtab
+        set smartindent
+        set rnu
+        let g:transparent_enabled = v:true
+      '';
 
-    plugins = {
-      nix = { enable = true; autoLoad = true; };
-      lightline = {
-        enable = true;
-        autoLoad = true;
-      };
-      web-devicons.enable = true; # required for barbar
-      barbar = {
-        enable = true;
-        autoLoad = true;
-        keymaps = with lib; {
-          close.key = "<A-x>";
-          closeAllButCurrent.key = "<A-S-x>";
-          first.key = "<A-S-h>";
-          last.key = "<A-S-l>"; 
-          next.key = "<A-l>";
-          previous.key = "<A-h>";
-          moveNext.key = "<A-S-k>";
-          movePrevious.key = "<A-S-j>";
-          goTo1.key = "<A-1>";
-          goTo2.key = "<A-2>";
-          goTo3.key = "<A-3>";
-          goTo4.key = "<A-4>";
-          goTo5.key = "<A-5>";
-          goTo6.key = "<A-6>";
-          goTo7.key = "<A-7>";
-          goTo8.key = "<A-8>";
-          goTo9.key = "<A-9>";
-        }; 
-      };
-      visual-multi = {
-        enable = true;
-        autoLoad = true;
-      };
-      neo-tree = {
-        enable = true;
-        closeIfLastWindow = true;
-        eventHandlers = {
-          file_opened = ''
-            function(file_path)
-              --auto close
-              require("neo-tree").close_all()
-            end
-          '';
+      colorschemes.${cfg.colorscheme}.enable = true;
+
+      plugins = {
+        nix = plug-on;
+        lightline = plug-on;
+        visual-multi = plug-on;
+        transparent = plug-on;
+
+        barbar = {
+          enable = true;
+          autoLoad = true;
+          keymaps = with lib; {
+            close.key = "<A-x>";
+            closeAllButCurrent.key = "<A-S-x>";
+            first.key = "<A-S-h>";
+            last.key = "<A-S-l>"; 
+            next.key = "<A-l>";
+            previous.key = "<A-h>";
+            moveNext.key = "<A-S-k>";
+            movePrevious.key = "<A-S-j>";
+            goTo1.key = "<A-1>";
+            goTo2.key = "<A-2>";
+            goTo3.key = "<A-3>";
+            goTo4.key = "<A-4>";
+            goTo5.key = "<A-5>";
+            goTo6.key = "<A-6>";
+            goTo7.key = "<A-7>";
+            goTo8.key = "<A-8>";
+            goTo9.key = "<A-9>";
+          }; 
         };
+        web-devicons.enable = true; # required for barbar
+
+        neo-tree = {
+          enable = true;
+          closeIfLastWindow = true;
+          eventHandlers = {
+            file_opened = ''
+              function(file_path)
+                --auto close
+                require("neo-tree").close_all()
+              end
+            '';
+          };
+        };
+
       };
     };
   };
