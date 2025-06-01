5cb5ddcc6d8bfedcc69b15cbe06d0d3b9f3d3744
Richard Carter <goobysc@gmail.com>
2025-05-31 22:04:50 -0400

    nixvim plugins: airline and barbar

--- apps/cli/editors.nix
+++ apps/cli/editors.nix
@@ -1,4 +1,4 @@
-{ configs, pkgs, ... }:
+{ configs, pkgs, lib, ... }:
 {
   users.users.richard.packages = with pkgs; [
     (spacevim.override {
@@ -10,5 +10,48 @@
     sv = "spacevim";
   };
 
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    vimAlias = true;
+    extraConfigVim = ''
+      set tabstop=2
+      set shiftwidth=2
+      set expandtab
+      set smartindent
+    '';
+
+    colorschemes.ayu.enable = true;
+
+    plugins = {
+      airline = {
+        enable = true;
+        autoLoad = true;
+      };
+      web-devicons.enable = true; # required for barbar
+      barbar = {
+        enable = true;
+        autoLoad = true;
+        keymaps = with lib; {
+          close.key = "<A-x>";
+          closeAllButCurrent.key = "<A-S-x>";
+          first.key = "<A-S-h>";
+          last.key = "<A-S-l>"; 
+          next.key = "<A-l>";
+          previous.key = "<A-h>";
+          moveNext.key = "<A-S-k>";
+          movePrevious.key = "<A-S-j>";
+          goTo1.key = "<A-1>";
+          goTo2.key = "<A-2>";
+          goTo3.key = "<A-3>";
+          goTo4.key = "<A-4>";
+          goTo5.key = "<A-5>";
+          goTo6.key = "<A-6>";
+          goTo7.key = "<A-7>";
+          goTo8.key = "<A-8>";
+          goTo9.key = "<A-9>";
+        }; 
+      };
+    };
+  };
 }
