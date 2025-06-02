1754f54f781681d02ae37b395d24d5097bea7f99
Richard Carter <goobysc@gmail.com>
2025-06-02 07:06:32 -0400

    remove some comments

--- hosts/snootflix/default.nix
+++ hosts/snootflix/default.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 let
   root-config-dir = ./../..;
@@ -68,11 +64,6 @@ in
   };
 
   users.users.richard.extraGroups = [ "networkmanager" "wheel" ];
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
