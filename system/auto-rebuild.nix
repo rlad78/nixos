bd814e54247ac9e60e42e86f7d310f7573ad1502
Richard Carter <goobysc@gmail.com>
2025-06-02 11:04:56 -0400

    add git pull script for auto-rebuild

--- system/auto-rebuild.nix
+++ system/auto-rebuild.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 let
   flake-dir = "/home/richard/nixos";
+
+  flake-pull-script = pkgs.writeShellApplication {
+    name = "flake-auto-pull";
+    runtimeInputs = [ pkgs.git ];
+    text = ''
+      git pull -C /home/richard/nixos
+    '';
+  };
 in
 {
   system.autoUpgrade = {
@@ -13,5 +21,14 @@ in
       lower = "02:00";
       upper = "05:00";
     };
+  };
+
+  systemd.user.services.pull-flake = {
+    serviceConfig.ExecStart = "${flake-pull-script}/bin/flake-auto-pull";
+  };
+
+  system.user.timers.pull-flake = {
+    wantedBy = [ "timers.target" ];
+    timerConfig.OnCalendar = "Mon *-*-* 01:00:00";
   };
 }
