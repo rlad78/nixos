635019fbc860b984626674974804c6f7edc6b92f
Richard Carter <goobysc@gmail.com>
2025-06-07 14:11:18 -0400

    filebrowser test

--- services/filebrowser.nix
+++ services/filebrowser.nix
@@ -1,34 +1,30 @@
 { config, pkgs, secrets, ... }:
 let
   filebrowser-uid = 7789;
-  filebrowser-gid = 9877;
-
   filebrowser-ext-port = 9007;
 in
 {
   virtualisation.docker.enable = true;
   users.users.richard.extraGroups = [ "docker" ];
 
-  users.groups.filebrowser.gid = filebrowser-gid;
-
   users.users.filebrowser = {
     isSystemUser = true;
-    group = "filebrowser";
+    group = "storage";
     uid = filebrowser-uid;
   };
 
   systemd.tmpfiles.rules = [
-    "d /filebrowser 0750 filebrowser filebrowser"
-    "d /filebrowser/srv 0750 filebrowser filebrowser"
-    "f /filebrowser/filebrowser.db 0600 filebrowser filebrowser"
-    "f /filebrowser/filebrowser.json 0600 filebrowser filebrowser"
+    "d /filebrowser 0750 filebrowser storage"
+    "d /filebrowser/srv 0750 filebrowser storage"
+    "f /filebrowser/filebrowser.db 0600 filebrowser storage"
+    "f /filebrowser/filebrowser.json 0600 filebrowser storage"
   ];
 
   virtualisation.oci-containers.backend = "docker";
   virtualisation.oci-containers.containers = {
     filebrowser = {
       image = "filebrowser/filebrowser:latest";
-      user = "${builtins.toString filebrowser-uid}:${builtins.toString filebrowser-gid}";
+      user = "${builtins.toString filebrowser-uid}:${builtins.toString config.users.groups.storage.gid}";
       autoStart = true;
       volumes = [
         "/filebrowser/srv:/srv"
