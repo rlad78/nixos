04913080e5993aa426e9cf8c1b5f618e86175a2c
Richard Carter <goobysc@gmail.com>
2025-06-12 15:17:33 -0400

    pre-configured firefox over zen

--- apps/desktop/internet.nix
+++ apps/desktop/internet.nix
@@ -1,12 +1,27 @@
-{ configs, pkgs, lib, ... }:
+{ pkgs, ... }:
 {
+  programs.firefox = {
+    enable = true;
+    policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      DisablePocket = true;
+      # DisplayBookmarksToolbar = "newtab";
+      OfferToSaveLogins = false;
+      ShowHomeButton = false;
+    };
+    preferences = {
+      "sidebar.verticalTabs" = true;
+      "sidebar.main.tools" = "history,bookmarks";
+    };
+  };
+
   services.flatpak.enable = true;
   services.flatpak.update.onActivation = true;
 
   services.flatpak.packages = [
     "one.jwr.interstellar"
     "com.bitwarden.desktop"
-    "app.zen_browser.zen"
     "org.chromium.Chromium"
     "dev.vencord.Vesktop"
     "org.telegram.desktop"
