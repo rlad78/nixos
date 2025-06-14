9204ae3be739ce032cee1206c8b8f6e5354574e9
Richard Carter <goobysc@gmail.com>
2025-06-14 16:01:31 -0400

    remove zed editor

--- apps/desktop/editors.nix
+++ apps/desktop/editors.nix
@@ -1,28 +1,5 @@
 { config, pkgs, ... }:
-# let
-  # extensions = nix-vscode-extensions.extensions.${pkgs.system};
-# in
 {
-  # users.users.richard.packages = with pkgs; [
-    # (vscode-with-extensions.override {
-      # vscodeExtensions = with extensions.vscode-marketplace; [
-        # ms-python.python
-        # aaron-bond.better-comments
-        # codezombiech.gitignore
-        # mathematic.vscode-pdf
-        # mechatroner.rainbow-csv
-        # ms-python.black-formatter
-        # ms-python.isort
-        # ms-python.vscode-pylance
-        # njpwerner.autodocstring
-        # piotrpalarz.vscode-gitignore-generator
-        # tamasfe.even-better-toml
-        # teabyii.ayu
-        # jnoortheen.nix-ide
-      # ];
-    # })
-  # ];
-
   environment.systemPackages = with pkgs; [
     poetry
     nixd
