5dd8b1e1f60825f9f3e0ae4c33fc538cfdf691d1
Richard Carter <goobysc@gmail.com>
2025-06-14 16:01:42 -0400

    use real vlc so it can be system default

--- apps/desktop/multimedia.nix
+++ apps/desktop/multimedia.nix
@@ -1,10 +1,13 @@
 { configs, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    vlc
+  ];
+
   services.flatpak.enable = true;
   services.flatpak.update.onActivation = true;
 
   services.flatpak.packages = [
-    "org.videolan.VLC"
     "com.spotify.Client"
     "com.github.PintaProject.Pinta"
   ];
