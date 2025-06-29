c2e02d087a02b4491b8939ffb32c04c52e0cce21
Richard Carter <goobysc@gmail.com>
2025-06-29 13:26:33 -0400

    make libreoffice stil and more dynamic for plasma

--- apps/desktop/productivity.nix
+++ apps/desktop/productivity.nix
@@ -1,7 +1,12 @@
 { configs, pkgs, lib, pomatez, ... }:
 {
   users.users.richard.packages = with pkgs; [
-    libreoffice-fresh
+    (if services.desktopManager.plasma6.enable == true
+      then libreoffice-qt6
+      else libreoffice
+    )
+    hunspell
+    hunspellDicts.en_US
   ];
 
   services.flatpak.enable = true;
