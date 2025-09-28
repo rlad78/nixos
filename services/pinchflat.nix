6946b2c9f87a43cec528f8e9f464d88303693ddc
Richard Carter <goobysc@gmail.com>
2025-09-27 21:08:57 -0400

    put pinchflat media on storage drive

--- services/pinchflat.nix
+++ services/pinchflat.nix
@@ -1,13 +1,25 @@
 { config , ...}:
+let
+  mediaTarget = "/storage/pinchflat";
+in
 {
   systemd.tmpfiles.rules = [
-    "L /pinchflat - - - - /var/lib/pinchflat"
+    # "L /pinchflat - - - - /var/lib/pinchflat"
+    "d ${mediaTarget} 0770 pinchflat storage"
   ];
+
+
+  users.users.pinchflat = {
+    # group = "storage";
+    isSystemUser = true;
+  };
 
   services.pinchflat = {
     enable = true;
     openFirewall = true;
     secretsFile = ./../secrets/pinchflat.txt;
     selfhosted = true;
+    mediaDir = mediaTarget;
+    user = "pinchflat";
   };
 }
