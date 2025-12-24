3207c591f5b4ee769cc2edd40ac7aff205b35315
Richard Carter <goobysc@gmail.com>
2025-12-24 17:13:47 -0500

    add sonarr-anime to snootflix role

--- roles/snootflix.nix
+++ roles/snootflix.nix
@@ -3,6 +3,13 @@ let
   root-config-dir = ./..;
   cfg = config.arf.snootflix;
 
+  sonarr-anime = {
+    config-dir = "${config.nixarr.stateDir}/sonarr-anime";
+    hostPort = 8981;
+    uid = 8981;
+    media-gid = config.users.groups.media.gid;
+  };
+
   my-device-ips = with lib; lists.unique (lists.flatten (
     builtins.map (a: attrsets.attrValues a) (
       builtins.map (x: attrsets.filterAttrs (
@@ -128,7 +135,78 @@ in
       };
     };
 
-    # add sonarr-anime
+    # sonarr-anime
+    users.users.sonarr-anime = {
+      isSystemUser = true;
+      group = "media";
+      uid = sonarr-anime.uid;
+    };
+
+    containers.sonarr-anime = let
+      fromHost = {
+        user = rec {
+          name = "sonarr-anime";
+          uid = sonarr-anime.uid;
+        };
+        group = rec {
+          name = "media";
+          gid = sonarr-anime.media-gid;
+        };
+        sonarr-package = pkgs.sonarr;
+        media-dir = builtins.toString cfg.mediaRootDir;
+      };
+    in {
+      autoStart = true;
+      privateNetwork = true;
+      hostAddress = "10.0.0.10";
+      localAddress = "10.0.0.69";
+      ephemeral = true;
+      bindMounts = {
+        "${fromHost.media-dir}" = {
+          hostPath = fromHost.media-dir;
+          isReadOnly = false;
+        };
+        "${sonarr-anime.config-dir}" = {
+          hostPath = sonarr-anime.config-dir;
+          isReadOnly = false;
+        };
+      };
+      forwardPorts = [
+        {
+          containerPort = 8989;
+          hostPort = sonarr-anime.hostPort;
+          protocol = "tcp";
+        }
+      ];
+
+      config = { config, pkgs, ... }: {
+        users = {
+          users."${fromHost.user.name}" = {
+            uid = fromHost.user.uid;
+            isSystemUser = true;
+            group = fromHost.group.name;
+          };
+          groups."${fromHost.group.name}" = {
+            gid = fromHost.group.gid;
+
+          };
+        };
+        services.sonarr = {
+          enable = true;
+          package = fromHost.sonarr-package;
+          openFirewall = true;
+          user = fromHost.user.name;
+          group = fromHost.group.name;
+          dataDir = "${sonarr-anime.config-dir}";
+        };
+
+        networking.useHostResolvConf = lib.mkForce false;
+        services.resolved.enable = true;
+
+        system.stateVersion = "25.11";
+      };
+    };
+
     # add jellarr
     # add unmanic
     # add janitorr
