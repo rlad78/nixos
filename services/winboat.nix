41647cc718058817b14df6389554ff37bdc92f75
Richard Carter <goobysc@gmail.com>
2025-12-26 09:46:45 -0500

    working winboat

--- services/winboat.nix
+++ services/winboat.nix
@@ -1,20 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   virtualisation = {
     libvirtd = {
       enable = true;
-      qemu.swtmp = {
-        enable = false;
-        package = with pkgs.stable; swtpm;
-      };
+      qemu.swtpm.enable = true;
     };
     spiceUSBRedirection.enable = true;
+    containers.enable = true;
+    podman = {
+      enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
   };
   users.users.richard.extraGroups = [ "libvirtd" ];
   services.spice-vdagentd.enable = true;
   programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
-    winboat
+    pkgs-unstable.winboat
+    podman-compose
   ];
 }
