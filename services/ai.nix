39a1897ec8f10c9c2ad7fb6ca2d9ae2a53459a76
Richard Carter <goobysc@gmail.com>
2026-03-15 20:04:18 -0400

    modernize ollama

--- services/ai.nix
+++ services/ai.nix
@@ -1,16 +1,23 @@
-{ config, ... }:
+{ config, pkgs-unstable, ... }:
 {
   services.ollama = {
     enable = true;
     acceleration = "cuda";
-    loadModels = [ "deepseek-r1:32b" ];
+    port = 11434;
+    openFirewall = true;
+    package = pkgs-unstable.ollama;
+    # loadModels = [ "deepseek-r1:32b" ];
   };
 
-  services.nextjs-ollama-llm-ui = {
-    enable = true;
-    hostname = "0.0.0.0";
-    port = 3000;
-  };
+  environment.systemPackages = with pkgs-unstable; [
+    llmfit
+  ];
 
-  networking.firewall.allowedTCPPorts = [ config.services.nextjs-ollama-llm-ui.port ];
+  # services.nextjs-ollama-llm-ui = {
+  #   enable = true;
+  #   hostname = "0.0.0.0";
+  #   port = 3000;
+  # };
+
+  # networking.firewall.allowedTCPPorts = [ config.services.nextjs-ollama-llm-ui.port ];
 }
