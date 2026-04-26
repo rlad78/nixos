5a5b8838ad2e7f59532544298d453a61026eece4
Richard Carter <goobysc@gmail.com>
2026-04-26 13:57:24 -0400

    add global docker image prune service

--- system/base.nix
+++ system/base.nix
@@ -1,36 +1,60 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-  config = {
-    # enable flakes
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+  config = lib.mkMerge [
+    {
+      # enable flakes
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
-    # pkgs required for flakes and shell
-    environment.systemPackages = with pkgs; [
-      git
-      curl
-      git-crypt
-      screen
-      poetry
-      gh
-      lazygit
-    ];
+      # pkgs required for flakes and shell
+      environment.systemPackages = with pkgs; [
+        git
+        curl
+        git-crypt
+        screen
+        poetry
+        gh
+        lazygit
+      ];
 
-    environment.shellAliases = {
-      lzgit = "lazygit";
-    };
-
-    programs.zsh = {
-      enable = true;
-      autosuggestions = {
-        enable = true;
-        strategy = [
-          "completion"
-          "history"
-        ];
+      environment.shellAliases = {
+        lzgit = "lazygit";
       };
-    };
-  };
+
+      programs.zsh = {
+        enable = true;
+        autosuggestions = {
+          enable = true;
+          strategy = [
+            "completion"
+            "history"
+          ];
+        };
+      };
+    }
+
+    (lib.mkIf config.virtualisation.docker.enable {
+      systemd.services.docker-image-prune = {
+        description = "Prune unused Docker images";
+        after = [ "docker.service" ];
+        wants = [ "docker.service" ];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = "${pkgs.docker}/bin/docker image prune -a -f";
+        };
+      };
+
+      systemd.timers.docker-image-prune = {
+        wantedBy = [ "timers.target" ];
+        description = "Monthly Docker image prune";
+        timerConfig = {
+          OnCalendar = "*-*-01 03:00:00";
+          Persistent = true;
+          Unit = "docker-image-prune.service";
+        };
+      };
+    })
+  ];
 }
