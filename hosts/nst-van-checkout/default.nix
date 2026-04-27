bd0c159e319ce87705d40a9cfed1745b19e6a362
Richard Lee Carter Jr <goobysc@gmail.com>
2026-04-27 14:53:40 -0400

    getting ready to move van-checkout back to dell laptop

--- hosts/nst-van-checkout/default.nix
+++ hosts/nst-van-checkout/default.nix
@@ -1,51 +1,16 @@
-{ config, pkgs, lib, ... }:
+{ lib, ... }:
 let
   root-config-dir = ./../..;
 in
 {
-  arf = {
-    gc = {
-      enable = true;
-      frequency = "monthly";
-      older-than = 14;
-    };
-  };
-
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./van-checkout.nix
     ] ++ lib.lists.forEach [
-        "/system/barebones.nix"
-        "/system/networking.nix"
-        "/system/auto-rebuild.nix"
-        "/services/sshd.nix"
-        "/desktop-env/plasma.nix"
-      ] (p: root-config-dir + p);
+      "/roles/nst-van-checkout.nix"
+      "/system/systemd-boot.nix"
+    ] (p: root-config-dir + p);
 
-  # Bootloader.
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable networking
-  networking.hostName = "nst-van-checkout"; # Define your hostname.
-  networking.networkmanager.enable = true;
-
-  # we need a browser
-  environment.systemPackages = with pkgs; [ firefox ];
-
-  # automatically log in to Plasma session
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "richard";
-  };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  networking.hostName = "nst-van-checkout";
+  system.stateVersion = "25.11";
 }
