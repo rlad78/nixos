b4b54254bb558a1cbc08108319a4f03248d073c0
Richard Carter <goobysc@gmail.com>
2026-09-11 18:45:52 -0400

    specifically use mongodb 7 for unifi

--- services/unifi.nix
+++ services/unifi.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.unifi = {
     enable = true;
     openFirewall = true;
+    unifiPackage = pkgs.unifi;
+    mongodbPackage = pkgs.mongodb-7_0;
   };
 
   networking.firewall.allowedTCPPorts = [ 8443 ];
