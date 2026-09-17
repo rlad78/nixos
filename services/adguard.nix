78e923eda4389c44f0645fb9fb49f54a4813f6db
Richard Carter <goobysc@gmail.com>
2026-09-17 15:24:02 -0400

    fix up adguard config

--- services/adguard.nix
+++ services/adguard.nix
@@ -1,8 +1,11 @@
 { ... }:
 {
-  servies.adguardhome = {
+  services.adguardhome = {
     enable = true;
     openFirewall = true;
     mutableSettings = true;
   };
+
+  networking.firewall.allowedUDPPorts = [ 53 ];
+  networking.firewall.allowedTCPPorts = [ 53 ];
 }
